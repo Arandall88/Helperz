2d643a23098a15d433ba16068e337e0fb830b1df
Angel Randall <drivenbysuccess21@gmail.com>
2020-05-15 15:46:59 -0500

    added responsiveness to layout, changed margin, centered text

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -5,10 +5,16 @@ import Help from "../../images/help.jpg";
 class Home extends Component {
   render() {
     return (
-
-
       <div id="helping-hands">
-
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 style={{ textAlign: "center", float: "none" }}>
+                Welcome to Helperz!
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
       // <div>
       //   Home Page.
