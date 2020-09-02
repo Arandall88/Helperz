e969d2d2155b742a4faa8ecffe282f57b3e2b934
Angel Randall <drivenbysuccess21@gmail.com>
2020-09-02 13:26:52 -0500

    changed nav to bootstrap navbar

--- client/src/components/shared/NavBar/NavBar.jsx
+++ client/src/components/shared/NavBar/NavBar.jsx
@@ -4,20 +4,47 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <div>
-      <nav>
-        <div className="nav-wrapper">
-          <a href="/" className="brand-logo">
-            Home
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="matches">Request Delivery</a>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
             </li>
-            <li>
-              <a href="dashboard">Dashboard</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
             </li>
-            <li>
-              <a href="complete-profile">Volunteer</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
             </li>
           </ul>
         </div>
